d0b110f42a7d8995fd93d2b4246c19a2c957b144
AndrewSukhobok95 <suha1253516@gmail.com>
2018-02-18 10:59:14 +0300

    exp

--- README.md
+++
@@ -1,4 +0,0 @@
-# SeekingAlpha_project
-Collection and Analysis of text from SeekingAlpha.com in financial project
-
-In collaboration with Ilya Dmitriev
--- raw_text_preprocessing/analytics_linking.py
+++ raw_text_preprocessing/analytics_linking.py
@@ -12,7 +12,8 @@ sares = pd.read_csv('data/res_tables/' + res_tabs[0])
 
 for f in res_tabs:
     sares = pd.read_csv('data/res_tables/' + f)
-    analutics_sa += sum(list(map(lambda x: x.split(', '), pd.unique(sares['Analysts_list']))), [])
+    analutics_sa += list(pd.unique(sares['Analyst']))
+    # analutics_sa += sum(list(map(lambda x: x.split(', '), pd.unique(sares['Analysts_list']))), [])
 
 analutics_sa = pd.unique(analutics_sa)
 
@@ -22,16 +23,14 @@ analutics_sa = analutics_sa[~pd.isnull(analutics_sa)].copy()
 # an = pd.read_stata('data/bran.dta')
 
 
+list(pd.unique(sares['Analyst']))
 
-
-det = pd.read_stata('data/det.dta', chunksize=100)
-
-
-det.__next__().columns
 
 
 
 recdet = pd.read_stata('data/recdet.dta')
+
+
 
 
 sample = recdet.iloc[[3,100,1007,4321, 70000, 5, 6, 20,  1678,62465,45114,9000,60000,43351], :].copy()
